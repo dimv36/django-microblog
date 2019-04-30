e74012939a71b59ba3259c787c1d10dd66d67373
Dmitry Voronin <carriigfate92@yandex.ru>
2019-04-30 09:07:41 +0300

    Render posts page via django templates

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('Hello, world')
+    user = {'username': 'testuser'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    context = {'user': user,
+               'posts': posts}
+    return render(request, 'index.html', context)
