db07b779ca010457297bfad38d6500cb4a9d1453
Dmitry Voronin <carriigfate92@yandex.ru>
2019-04-30 09:23:21 +0300

    Author & Post models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Author(AbstractUser):
+    pass
+
+
+class Post(models.Model):
+    body = models.TextField('Post text', max_length=140)
+    timestamp = models.DateTimeField(db_index=True, auto_now_add=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __repr__(self):
+        return '<Post: {} author: {}>'.format(self.body, self.user.username)
--- microblog/settings.py
+++ microblog/settings.py
@@ -100,7 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+AUTH_USER_MODEL = 'blog.Author'
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
